642b1efb0d894d5d9de2fdfc500300facf255060
Котенко Дмитрий <DKotenko@lanit>
2019-09-04 16:17:07 +0300

    'to\memcpy'

---
+++ ft_bzero.c
@@ -0,0 +1,15 @@
+#include <string.h>
+
+void bzero(void *s, size_t n)
+{
+    size_t i;
+    char *s;
+        
+    s = (char *)destination;    
+    i = 0;
+    while (i < n)
+    {
+        s[i] = '\0';
+        i++;
+    }
+}
---
+++ ft_memcpy.c
@@ -0,0 +1,11 @@
+void *memcpy(void *dest, const void *src, size_t n)
+{
+	size_t i;
+	
+	i = 0;
+	while(i < n)
+	{
+		((unsigned char*)dest)[i] = ((unsigned char*)src)[i];
+		i++;
+	}
+}
