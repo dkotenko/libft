11ae226f12edeeb48fef852c82801bd83b147694
Cooper Lala <clala@ga-l4.21-school.ru>
2019-09-07 20:28:08 +0300

    strrchr

---
+++ ft_memcmp.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clala <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/07 19:59:44 by clala             #+#    #+#             */
+/*   Updated: 2019/09/07 20:10:55 by clala            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int memcmp( const void * memptr1, const void * memptr2, size_t num )
+{
+	size_t i;
+
+	i = 0;
+	while(memptr1[i] && (char )*memptr2[i])
+	{
+		if((char *)memptr1[i] > (char *)memptr2[i])
+			return (char *)memptr1[i] - (char *)memptr2[i];
+		else if ((char *)memptr1[i] < (char *)memptr2[i])
+			return (char *)memptr1[i] - (char *)memptr2[i];
+		i++;
+	}
+	return 0;
