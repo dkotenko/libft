ce95ccac75ae7f6e5cb0635147057cce80870b06
Cooper Lala <clala@il-k5.msk.21-school.ru>
2021-02-10 19:53:27 +0300

    dlist free

---
+++ dlist/dlist_free.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   dlist_free.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: clala <clala@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/02/15 22:01:36 by clala             #+#    #+#             */
+/*   Updated: 2021/02/10 19:52:52 by clala            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "dlist.h"
+
+
+void			t_dlist_node_free(void (*free_func(t_dlist_node *)),
+	t_dlist_node *n)
+{
+	(*free_func)(n);
+}
+
+void			t_dlist_free(t_dlist *dlist)
+{
+	t_dlist_node	*node;
+	t_dlist_node	*next;
+
+	node = dlist->head;
+	while (node)
+	{
+		next = node->next;
+		t_dlist_node_free(t_dlist_node_free, node);
+		node = next;
+	}
+}
