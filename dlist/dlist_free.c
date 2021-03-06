d8b20a43d75ad3747e04de18bbc4997b34f379b8
Cooper Lala <clala@il-d1.msk.21-school.ru>
2021-03-06 20:41:56 +0300

    .mak file

--- dlist/dlist_free.c
+++ dlist/dlist_free.c
@@ -6,7 +6,7 @@
 /*   By: clala <clala@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/15 22:01:36 by clala             #+#    #+#             */
-/*   Updated: 2021/02/10 19:52:52 by clala            ###   ########.fr       */
+/*   Updated: 2021/02/21 01:24:03 by clala            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,17 @@ void				t_dlist_free(t_dlist *dlist,
 {
 	t_dlist_node	*node;
 	t_dlist_node	*next;
-
+	
+	
 	node = dlist->head;
+	
+	(void)free_func;
+	
 	while (node)
 	{
 		next = node->next;
-		t_dlist_node_free(free_func, node);
+		//t_dlist_node_free(free_func, node);
 		node = next;
 	}
+	
 }
